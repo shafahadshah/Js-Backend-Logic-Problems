cdff9019f268bea0048f71a95f37bf0c4b1687cf
shafahadshah <pirshahfahadshah@gmail.com>
2026-01-23 12:10:07 +0500

    Solve Problem 38

--- Beginner Logic Problems/script.js
+++ Beginner Logic Problems/script.js
@@ -486,3 +486,15 @@ union([1,2,3], [3,4,5]); // [1,2,3,4,5]
 const flatten2D = arr => arr.flat();
 // Example
 flatten2D([[1,2],[3,4],[5]]); // [1,2,3,4,5]
+
+// Problem 38 Sum of Diagonal of Matrix
+const diagonalSum = matrix => {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    sum += matrix[i][i];
+  }
+  return sum;
+};
+
+// Example
+diagonalSum([[1,2,3],[4,5,6],[7,8,9]]); // 15
