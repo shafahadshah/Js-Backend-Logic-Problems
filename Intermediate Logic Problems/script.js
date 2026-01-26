d26c9a60779b3cfe353255ff311909f3e7e0e465
shafahadshah <pirshahfahadshah@gmail.com>
2026-01-26 15:24:58 +0500

    Problem 51 Reverse String

---
+++ Intermediate Logic Problems/script.js
@@ -0,0 +1,7 @@
+//  Problem 51 Reverse String
+function reverseString(str) {
+  if (str.length <= 1) return str;
+  return reverseString(str.slice(1)) + str[0];
+}
+
+console.log(reverseString("hello")); // olleh
