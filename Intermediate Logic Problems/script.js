e1d213b4019df870d0346330453a48e752784d2c
shafahadshah <pirshahfahadshah@gmail.com>
2026-01-26 15:27:59 +0500

    Problem 55 Palindrome Check

--- Intermediate Logic Problems/script.js
+++ Intermediate Logic Problems/script.js
@@ -33,3 +33,14 @@ function countVowels(str, i = 0) {
 }
 
 console.log(countVowels("Javascript")); // 3
+
+//  Problem 55 Palindrome Check
+function isPalindrome(str, left = 0, right = str.length - 1) {
+  if (left >= right) return true;
+  if (str[left] !== str[right]) return false;
+
+  return isPalindrome(str, left + 1, right - 1);
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
