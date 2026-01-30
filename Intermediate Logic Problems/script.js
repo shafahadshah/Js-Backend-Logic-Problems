fed138ac1492e60d581739135dca85f402a0bd61
shafahadshah <pirshahfahadshah@gmail.com>
2026-01-30 15:13:16 +0500

    Problem 73 Manual filter()

--- Intermediate Logic Problems/script.js
+++ Intermediate Logic Problems/script.js
@@ -303,3 +303,24 @@ const mapArr = [1, 2, 3, 4];
 const mapResult = manualMap(mapArr, x => x * 2);
 
 console.log(mapResult); // [2, 4, 6, 8]
+
+
+//  Problem 73 Manual filter()
+function manualFilter(arr, callback) {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
+}
+
+// CALL
+const filterArr = [1, 2, 3, 4, 5, 6];
+const filterResult = manualFilter(filterArr, x => x % 2 === 0);
+
+console.log(filterResult); // [2, 4, 6]
+
