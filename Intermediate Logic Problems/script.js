2adb33201443e2a52a075b01cdc8f5c8ecb08f26
shafahadshah <pirshahfahadshah@gmail.com>
2026-02-01 12:13:51 +0500

    Problem 87 Detect Cycle in Graph (Directed)

--- Intermediate Logic Problems/script.js
+++ Intermediate Logic Problems/script.js
@@ -600,3 +600,40 @@ function dfs(root) {
 
 // Call
 console.log(dfs(tree));
+
+
+
+//  Problem 87 Detect Cycle in Graph (Directed)
+function hasCycle(graph) {
+  const visited = new Set();
+  const stack = new Set();
+
+  function dfs(node) {
+    if (stack.has(node)) return true;
+    if (visited.has(node)) return false;
+
+    visited.add(node);
+    stack.add(node);
+
+    for (const neighbor of graph[node] || []) {
+      if (dfs(neighbor)) return true;
+    }
+
+    stack.delete(node);
+    return false;
+  }
+
+  for (const node in graph) {
+    if (dfs(node)) return true;
+  }
+  return false;
+}
+
+// Call
+const graph = {
+  A: ["B"],
+  B: ["C"],
+  C: ["A"]
+};
+
+console.log(hasCycle(graph));
