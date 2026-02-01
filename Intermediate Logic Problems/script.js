6090e35e2e62db927751c212c538f619e06a0c34
shafahadshah <pirshahfahadshah@gmail.com>
2026-02-01 12:11:28 +0500

    Problem 84 Spiral Order Traversal of Matrix

--- Intermediate Logic Problems/script.js
+++ Intermediate Logic Problems/script.js
@@ -509,3 +509,40 @@ console.log(rotateAntiClockwise([
   [4,5,6],
   [7,8,9]
 ]));
+
+
+//  Problem 84 Spiral Order Traversal of Matrix
+function spiralOrder(matrix) {
+  if (!matrix.length) return [];
+
+  let top = 0, bottom = matrix.length - 1;
+  let left = 0, right = matrix[0].length - 1;
+  const result = [];
+
+  while (top <= bottom && left <= right) {
+    for (let i = left; i <= right; i++) result.push(matrix[top][i]);
+    top++;
+
+    for (let i = top; i <= bottom; i++) result.push(matrix[i][right]);
+    right--;
+
+    if (top <= bottom) {
+      for (let i = right; i >= left; i--) result.push(matrix[bottom][i]);
+      bottom--;
+    }
+
+    if (left <= right) {
+      for (let i = bottom; i >= top; i--) result.push(matrix[i][left]);
+      left++;
+    }
+  }
+
+  return result;
+}
+
+// Call
+console.log(spiralOrder([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+]));
